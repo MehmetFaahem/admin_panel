88ef77dd4c066b729aa94a3e58af3334584f464d
mehmetfaahem <mehmetfaahem@gmail.com>
2024-09-06 21:15:34 +0600

    button align fixed

--- components/AdminLayout.js
+++ components/AdminLayout.js
@@ -28,9 +28,10 @@ export default function AdminLayout({ children }) {
 
   if (!isLoggedIn) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-white p-6 rounded shadow-md w-full max-w-sm">
-          <h2 className="text-2xl font-bold mb-4">Admin Panel</h2>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h2 className="text-2xl font-bold mb-4">Admin Panel</h2>
+
+        <div className="bg-white p-6 rounded shadow-md w-full max-w-sm ">
           <Link href="/login">
             <button className="w-full py-2 mb-4 bg-black text-white rounded">
               Login
